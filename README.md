af8cc29fcf696831684af91109526603a24f6d8e
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2013-10-23 12:29:22 +0800

    add share-property problem

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ hole.js
 =======
 
 Problem I come across with the years deal with Javascript language, also including dialect of Javascript such as CoffeeScript
+
+codestyle
+=========
+
+Nearly every file will including problem and solution code.
+Problem code will be prefix with `$` to distinguish from correct code.
---
+++ hole/class/share-property.js
@@ -0,0 +1,47 @@
+/**
+ * Platform cattail@cattop.cattail.me Linux3.11.5-1-ARCH@x86_64 GNU/Linux
+ * Date 2013-10-23 12:19:43
+ * @author zhongchiyu@gmail.com (cattail)
+ * @description Instance property could be share accidently when using 
+ * Javascript's prototype based class inheritance.
+ *
+ * ## Reference
+ * * [coffeescript-class-member](http://stackoverflow.com/questions/8355371/coffeescript-class-members)
+ */
+
+// If property is primitive value, it will be fine.
+function Klass () {}
+Klass.prototype.prop = 0;
+Klass.prototype.setProp = function (value) {
+  this.prop = value;
+};
+var obj1 = new Klass();
+var obj2 = new Klass();
+obj2.setProp(1);
+console.log(obj1.prop === 0)
+console.log(obj2.prop === 1);
+
+// Problem comes with non-primitive value
+function $Set () {}
+$Set.prototype.items = [];
+$Set.prototype.add = function (item) {
+  this.items.push(item);
+};
+var obj3 = new $Set();
+var obj4 = new $Set();
+obj4.add(1);
+console.log(obj3.items.length === 1);
+console.log(obj4.items.length === 1);
+
+// Solution is to move property initialization into constructor
+function Set () {
+  this.items = [];
+}
+Set.prototype.add = function (item) {
+  this.items.push(item);
+};
+var obj5 = new Set();
+var obj6 = new Set();
+obj6.add(1);
+console.log(obj5.items.length === 0);
+console.log(obj6.items.length === 1);
